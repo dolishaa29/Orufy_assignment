efec744c01b0869fc5d103661136d4c54e225dcb
dolisha <dolishagandhi@gmail.com>
2026-05-15 01:36:35 +0530

    new

---
+++ Backend/router/user.js
@@ -0,0 +1,8 @@
+let usermodel=require("../model/user");
+const express = require("express");
+let router = express.Router();
+
+router.post("/register",);
+router.post("/login",);
+
+module.exports=router;
---
+++ Backend/service/user.js
@@ -0,0 +1,34 @@
+let rec=require("../model/user");
+let jwt=require("jsonwebtoken");
+let bct=require("bcrypt");
+
+exports.register=async(req,res)=>{
+    try{
+        let name=req.body.name;
+        let email=req.body.email;
+        let password=req.body.password;
+        let contact=req.body.contact;
+        let address=req.body.address;
+
+        let user=await rec.findOne({email:email});
+        if(user)
+        {
+            return res.status(400).json({msg: "User already exists"});
+        }
+        else{
+            let hash=await bct.hash(password,10);
+            let user=new rec({name:name,email:email,password:hash,contact:contact,address:address});
+            await user.save();
+            res.status(201).json({msg: "User registered successfully"});
+        }
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({
+            error:"Internal Server Error",
+            message:err.message,
+        });
+    }
+}
+
