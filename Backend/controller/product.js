511c4d82d6c02afaea6118d83f071513dd8721e8
dolisha <dolishagandhi@gmail.com>
2026-05-17 14:42:28 +0530

    new updates

--- Backend/controller/product.js
+++ Backend/controller/product.js
@@ -1,4 +1,4 @@
-const { addproduct, viewproduct, editproduct, deleteproduct } = require("../service/product")
+const { addproduct, viewproduct, editproduct, deleteproduct, explore, singleproduct, singleproductexplore } = require("../service/product")
 
 exports.addproduct=async(req,res)=>
 {
@@ -18,4 +18,14 @@ exports.editproduct=async(req,res)=>
 exports.deleteproduct=async(req,res)=>
 {
     await deleteproduct(req,res);
+}
+
+exports.explore=async(req,res)=>
+{
+    await explore(req,res);
+}
+
+exports.singleproductexplore=async(req,res)=>
+{
+    await singleproductexplore(req,res);
 }
